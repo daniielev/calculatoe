03609fa97f000b8a10e985efa5ba3db170f1538c
Daniel Muñoz <daniel@danielmunoz.cr>
2016-02-21 02:40:48 -0600

    localhost guideline

--- js/CalculatorService.js
+++ js/CalculatorService.js
@@ -1,18 +1,18 @@
 angular.module('calculatorApp', [])
     // Set the back-end common URL
-    .value('URLBase', 'https://danielmunnoz.com/cenfotec/calculator/controller.php')
+    .value('BaseURL', 'https://danielmunnoz.com/cenfotec/calculator/controller.php') // Set this to "/controller.php" when using localhost
     /**
      * Define the service which will be provided by the Back-end
      */
-    .service('CalculatorService', ['$http', 'URLBase',
-        function($http, URLBase) {
+    .service('CalculatorService', ['$http', 'BaseURL',
+        function($http, BaseURL) {
             /**
              * Builds the URL to call the method of the Calculator
              *
              * @returns {*} - The result of the operation
              */
             var perform = function (operator, firstDigit, secondDigit) {
-                var url = URLBase + '?operation=' + operator + '&firstDigit=' + firstDigit + '&secondDigit=' + secondDigit;
+                var url = BaseURL + '?operation=' + operator + '&firstDigit=' + firstDigit + '&secondDigit=' + secondDigit;
 
                 return $http({
                     method: "GET",
