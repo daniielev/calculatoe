9cbb3d2332289d950bec22ca5345c6914feb6380
Daniel Muñoz <daniel@danielmunoz.cr>
2016-02-20 22:23:05 -0600

    Calculator service in PHP completed. -V1

--- CalculatorService.php
+++ CalculatorService.php
@@ -6,6 +6,8 @@
 * @constructor
 */
 class CalculatorService {
+
+    // Available functions
     const ADDITION = 'addition';
     const SUBTRACTION = 'subtraction';
     const MULTIPLICATION = 'multiplication';
@@ -21,9 +23,78 @@ class CalculatorService {
      * @param {integer} $secondValue - The first digit from the operation
      * @return {integer}
      */
-    function addition($firstVale, $secondValue) {
-        if (isRequestValid()) {
-            return $firstVale . $secondValue;
+    function addition ($firstVale, $secondValue) {
+        if ($this->isRequestValid()) {
+            return ($firstVale + $secondValue);
+        }
+    }
+
+     /**
+     * Takes the first digit and the second one and performs
+     * a subtraction of both numbers.
+     *
+     * @param {integer} $firstVale - The first digit from the operation
+     * @param {integer} $secondValue - The first digit from the operation
+     * @return {integer}
+     */
+    function subtraction ($firstVale, $secondValue) {
+        if ($this->isRequestValid()) {
+            return ($firstVale - $secondValue);
+        }
+    }
+
+    /**
+     * Takes the first digit and the second one and performs
+     * a multiplication of both numbers.
+     *
+     * @param {integer} $firstVale - The first digit from the operation
+     * @param {integer} $secondValue - The first digit from the operation
+     * @return {integer}
+     */
+    function multiplication ($firstVale, $secondValue) {
+        if ($this->isRequestValid()) {
+            return ($firstVale * $secondValue);
+        }
+    }
+
+    /**
+     * Takes the first digit and the second one and performs
+     * a division of both numbers.
+     *
+     * @param {integer} $firstVale - The first digit from the operation
+     * @param {integer} $secondValue - The first digit from the operation
+     * @return {integer}
+     */
+    function division ($firstVale, $secondValue) {
+        if ($this->isRequestValid()) {
+            /**
+             * PHP shows 'PHP Warning:  Division by zero' when
+             * sending the second value as 0 (zero). We show
+             * an error then.
+             */
+            if (!$secondValue == 0) {
+                return ($firstVale / $secondValue);
+            } else {
+                return "ERROR, DIVISION";
+            }
+        }
+    }
+
+     /**
+     * Takes the first digit and the second one and performs
+     * a modulus of both numbers.
+     *
+     * @param {integer} $firstVale - The first digit from the operation
+     * @param {integer} $secondValue - The first digit from the operation
+     * @return {integer}
+     */
+    function modulus ($firstVale, $secondValue) {
+        if ($this->isRequestValid()) {
+            if (!$secondValue == 0) {
+                return ($firstVale % $secondValue);
+            } else {
+                return "ERROR, MODULUS";
+            }
         }
     }
 
@@ -33,7 +104,7 @@ class CalculatorService {
      *
      * @return {boolean}
      */
-    public function isRequestValid() {
+    public function isRequestValid () {
         $flag = false;
 
         if ((isset($_REQUEST['operation']) and $_REQUEST['operation'] != '') and
--- controller.php
+++ controller.php
@@ -1,6 +1,6 @@
 <?php
 // Includes the Calculator logic
-require("CalculatorService.php");
+require('CalculatorService.php');
 
 // Get an instance of the Calculator class
 $calculator = new CalculatorService();
@@ -8,6 +8,93 @@ $calculator = new CalculatorService();
 // Sets a flag to check is the operation should be perform
 $isRequestValid = $calculator->isRequestValid();
 
-// echo(var_dump($isRequestValid));
+// Sets the global variables for the operations
+if ($isRequestValid) {
+    // Get the digits to be used
+    $numbers = prepareData();
+    $firstDigit = $numbers[0];
+    $secondDigit = $numbers[1];
+
+    $operationToPerform = $_REQUEST['operation'];
+    $result;
+
+    /**
+     * Calls the calculator opertation depending on the operator sent,
+     * then set the $result with the result of the operation
+     */
+    switch ($operationToPerform) {
+        case 'addition':
+            if ($operationToPerform == $calculator::ADDITION) {
+                $result = $calculator->addition($firstDigit, $secondDigit);
+            }
+            break;
+
+        case 'subtraction':
+            if ($operationToPerform == $calculator::SUBTRACTION) {
+                $result = $calculator->subtraction($firstDigit, $secondDigit);
+            }
+            break;
+
+        case 'multiplication':
+            if ($operationToPerform == $calculator::MULTIPLICATION) {
+                $result = $calculator->multiplication($firstDigit, $secondDigit);
+            }
+            break;
+
+        case 'division':
+            if ($operationToPerform == $calculator::DIVISION) {
+                $result = $calculator->division($firstDigit, $secondDigit);
+            }
+            break;
+
+        case 'modulus':
+            if ($operationToPerform == $calculator::MODULUS) {
+                $result = $calculator->modulus($firstDigit, $secondDigit);
+            }
+            break;
+
+        // If the function sent does not exist, returns an error
+        default:
+            $result = "NOT METHOD";
+            break;
+    };
+
+    // Returns the result of the operation, then exit the current program
+    echo $result;
+    die();
+}
+
+/**
+     * Avoids data type issues. Evaluates if the data
+     * provided from the controller is numeric, if not
+     * then it will set the two digits to integer type.
+     *
+     * @return {array}
+     */
+function prepareData () {
+    $data = [];
+
+    $firstDigit = $_REQUEST['firstDigit'];
+
+    // If the second value is not present on the request, then its asumed it is 0 (zero)
+    $secondDigit = $_REQUEST['secondDigit'] || 0;
+
+    // Converts the first value to integer
+    if (!is_int($firstDigit)) {
+        $firstDigit = (int)$firstDigit;
+    }
+
+    // Converts the second value to integer
+    if (!is_int($secondDigit)) {
+        $secondDigit = (int)$secondDigit;
+    }
+
+    // Pushes the parsed values to the $data array
+    array_push($data, $firstDigit);
+    array_push($data, $secondDigit);
+
+    // Returns both values, NOW integer as type
+    return $data;
+}
 
 ?>
